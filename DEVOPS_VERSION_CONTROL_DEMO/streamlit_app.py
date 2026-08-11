a7f068c596f37a99521ec3eff6172a1527b6a9cd
Umair9009 <umair9009@hotmail.com>
2026-08-11 19:20:47 +0000

    fix check

--- DEVOPS_VERSION_CONTROL_DEMO/streamlit_app.py
+++ DEVOPS_VERSION_CONTROL_DEMO/streamlit_app.py
@@ -4,4 +4,4 @@ st.title("DevOps Version Control Demo")
 
 st.write("This change was developed on a feature branch.")
 
-st.button("Test 1.1.3 DevOps Workflow, Intentional failure, validation check" 
+st.button("Test 1.1.3 DevOps Workflow, Intentional failure, validation check, now fixed") 
